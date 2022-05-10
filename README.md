dc8f5ab7967990e81fa06adc31fa6523e226f863
Wallace Júnior <wflj1997@hotmail.com>
2022-05-10 16:15:07 -0300

    setup monorepo and packages/web

---
+++ README.md
@@ -0,0 +1,1 @@
+# Pelando Clone
---
+++ packages/web/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-orange-600 font-medium">Hello World</h1>
+    </div>
+  );
+};
+
+export default Home;
