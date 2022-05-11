2a748287504afc90549b8ddcfc7057d0ced29e0a
Wallace Júnior <wflj1997@hotmail.com>
2022-05-11 17:12:14 -0300

    setup de cores e icons do frontend

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,8 +1,37 @@
 import "@/styles/globals.css";
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { Fragment } from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App: NextPage<AppProps> = ({ Component, pageProps }) => (
+  <Fragment>
+    <Head>
+      <title>Pelando Clone</title>
 
-export default MyApp;
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+    </Head>
+
+    <div className="flex min-h-screen flex-col text-default-foreground dark:text-dark-default-foreground bg-default-background dark:bg-dark-default-background">
+      <Component {...pageProps} />
+
+      <noscript>
+        É necessário ter{" "}
+        <a
+          href="https://support.google.com/adsense/answer/12654?hl=pt-BR"
+          target="_blank"
+        >
+          JavaScript
+        </a>{" "}
+        ativado para interagir com esta página.
+      </noscript>
+    </div>
+  </Fragment>
+);
+
+export default App;
--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,11 +1,9 @@
 import type { NextPage } from "next";
 
-const Home: NextPage = () => {
-  return (
-    <div>
-      <h1 className="text-3xl text-orange-600 font-medium">Hello World</h1>
-    </div>
-  );
-};
+const Home: NextPage = () => (
+  <div>
+    <h1 className="text-2xl font-bold">Pelando</h1>
+  </div>
+);
 
 export default Home;
