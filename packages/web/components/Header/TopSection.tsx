aa20f991ad9f38e09900bd0b61acb79c677cd3ef
Wallace Júnior <wflj1997@hotmail.com>
2022-05-12 20:26:58 -0300

    responsividade de header top section

--- packages/web/components/Header/TopSection.tsx
+++ packages/web/components/Header/TopSection.tsx
@@ -5,9 +5,9 @@ import { SearchIcon } from "../icons/SearchIcon";
 import { Logo } from "../Logo";
 
 export const TopSection: React.FC = () => (
-  <div className="flex justify-between gap-14 pt-3">
-    <div className="flex items-center gap-4">
-      <button className="cursor-pointer">
+  <div className="xs:gap-2 flex gap-1 pt-2 sm:pt-3 md:justify-between md:gap-14">
+    <div className="flex flex-grow items-center gap-4 md:flex-grow-0">
+      <button className="hidden cursor-pointer md:block">
         <HamburguerIcon className="w-6" />
       </button>
 
@@ -18,15 +18,15 @@ export const TopSection: React.FC = () => (
       </Link>
     </div>
 
-    <div className="relative my-auto flex-grow">
+    <div className="relative md:my-auto md:flex-grow">
       <input
         type="text"
-        className="bg-input-background dark:bg-dark-input-background w-full rounded-full py-1.5 pr-9 pl-4 focus:outline-none"
+        className="bg-input-background dark:bg-dark-input-background hidden w-full rounded-full py-1.5 pr-9 pl-4 focus:outline-none md:block"
         placeholder="Procure por promoções ou lojas"
         autoComplete="off"
       />
 
-      <button className="absolute inset-y-0 right-1.5 cursor-pointer rounded-full">
+      <button className="bg-input-background dark:bg-dark-input-background absolute right-1 flex aspect-square w-9 cursor-pointer items-center justify-center rounded-full md:inset-y-0">
         <SearchIcon className="w-6" />
       </button>
     </div>
