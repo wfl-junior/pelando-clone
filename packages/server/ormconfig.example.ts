7bd820d045225d3aacce81e1ebd11b9e034153f3
Wallace Júnior <wflj1997@hotmail.com>
2022-05-22 14:19:05 -0300

    trocar database de mysql para sqlite

--- packages/server/ormconfig.example.ts
+++ packages/server/ormconfig.example.ts
@@ -2,15 +2,10 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 import path from "path";
 
 export const ormconfig: TypeOrmModuleOptions = {
-  type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "root",
-  password: "root",
-  database: "test",
+  type: "sqlite",
+  database: path.join(__dirname, "database.sqlite"),
   entities: [path.join(__dirname, "src", "entities", "*.entity{.ts,.js}")],
   migrations: [path.join(__dirname, "src", "migrations", "*{.ts,.js}")],
   synchronize: false,
   logging: false,
-  debug: false,
 };
--- packages/server/src/seeders/index.ts
+++ packages/server/src/seeders/index.ts
@@ -1,11 +1,22 @@
 import { ormconfig } from "@/ormconfig";
-import { DataSource } from "typeorm";
+import path from "path";
+import { DataSource, DataSourceOptions } from "typeorm";
 import { seedCategories } from "./categories/category.seeder";
 import { seedProducts } from "./products/product.seeder";
 import { seedStores } from "./stores/store.seeder";
 
 async function seed() {
-  await new DataSource(ormconfig as any).initialize();
+  const options = { ...ormconfig } as DataSourceOptions;
+
+  if (ormconfig.type === "sqlite") {
+    Object.assign(options, {
+      // se for sqlite tem que pegar o database de dist
+      database: path.join(__dirname, "..", "..", "dist", "database.sqlite"),
+    });
+  }
+
+  await new DataSource(options).initialize();
+
   await Promise.all([seedStores(), seedCategories()]);
   await seedProducts();
 
