5804129e2ce9ecefe05507ce07c158602256e3a5
Wallace Júnior <wflj1997@hotmail.com>
2022-05-22 16:05:50 -0300

    refatorar e ajustar layout de UserSection/UserButtons em Header/TopSection

--- packages/web/components/Header/TopSection/UserSection/UserButtons/index.tsx
+++ packages/web/components/Header/TopSection/UserSection/UserButtons/index.tsx
@@ -8,14 +8,14 @@ import { NotificationsButton } from "./NotificationsButton";
 export const UserButtons: React.FC = () => (
   <div className="flex items-center gap-2">
     <Link href="#">
-      <a className="hover:text-tertiary-foreground flex items-center py-1.5 px-1 font-bold transition-colors">
+      <a className="hover:text-primary hidden items-center gap-0.5 py-1.5 px-1 font-bold transition-colors md:flex">
         <PostarIcon className="w-6" />
         <span className="sr-only lg:not-sr-only">Postar</span>
       </a>
     </Link>
 
     <Link href="#">
-      <a className="hover:text-tertiary-foreground flex items-center py-1.5 px-1 font-bold transition-colors">
+      <a className="hover:text-primary hidden items-center gap-0.5 py-1.5 px-1 font-bold transition-colors md:flex">
         <AlertasIcon className="w-6" />
         <span className="sr-only lg:not-sr-only">Alertas</span>
       </a>
