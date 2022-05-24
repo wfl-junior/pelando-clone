d28f4cd013653e1ad43a1add83255b4747c3f4bc
Wallace Júnior <wflj1997@hotmail.com>
2022-05-23 22:36:11 -0300

    abrir modal de register/login ao clicar nos buttons de vote de product card caso não esteja logado

--- packages/web/components/MainPage/ProductCard/TemperatureSection/ColdButton.tsx
+++ packages/web/components/MainPage/ProductCard/TemperatureSection/ColdButton.tsx
@@ -1,12 +1,16 @@
 import { ColdIcon } from "@/components/icons/product-card/ColdIcon";
+import { useModalContext } from "@/contexts/ModalContext";
 import { useProductCardContext } from "@/contexts/ProductCardContext";
 import { useVoteOnProductMutation } from "@/hooks/apollo/useVoteOnProductMutation";
+import { useUser } from "@/hooks/useUser";
 import { authorizationHeaderWithToken } from "@/utils/accessToken";
 import React from "react";
 
 export const ColdButton: React.FC = () => {
   const [vote] = useVoteOnProductMutation();
   const { product } = useProductCardContext();
+  const { isLoggedIn } = useUser();
+  const { toggleModal } = useModalContext();
 
   return (
     <button
@@ -14,6 +18,10 @@ export const ColdButton: React.FC = () => {
       title="Esfriar"
       className="hover:bg-secondary-background hover:text-blue flex aspect-square w-6 items-center justify-center rounded-full transition-colors"
       onClick={async () => {
+        if (!isLoggedIn) {
+          return toggleModal(true, "register-login");
+        }
+
         try {
           await vote({
             variables: {
--- packages/web/components/MainPage/ProductCard/TemperatureSection/HotButton.tsx
+++ packages/web/components/MainPage/ProductCard/TemperatureSection/HotButton.tsx
@@ -1,12 +1,16 @@
 import { HotIcon } from "@/components/icons/product-card/HotIcon";
+import { useModalContext } from "@/contexts/ModalContext";
 import { useProductCardContext } from "@/contexts/ProductCardContext";
 import { useVoteOnProductMutation } from "@/hooks/apollo/useVoteOnProductMutation";
+import { useUser } from "@/hooks/useUser";
 import { authorizationHeaderWithToken } from "@/utils/accessToken";
 import React from "react";
 
 export const HotButton: React.FC = () => {
   const [vote] = useVoteOnProductMutation();
   const { product } = useProductCardContext();
+  const { isLoggedIn } = useUser();
+  const { toggleModal } = useModalContext();
 
   return (
     <button
@@ -14,6 +18,10 @@ export const HotButton: React.FC = () => {
       title="Esquentar"
       className="hover:bg-secondary-background hover:text-primary flex aspect-square w-6 items-center justify-center rounded-full transition-colors"
       onClick={async () => {
+        if (!isLoggedIn) {
+          return toggleModal(true, "register-login");
+        }
+
         try {
           await vote({
             variables: {
