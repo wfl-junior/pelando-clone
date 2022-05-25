008d06623c6821ec620fdd702c2aed1fe4c83f90
Wallace Júnior <wflj1997@hotmail.com>
2022-05-25 00:47:53 -0300

    refatorar transformDatesToString util

--- packages/server/test/utils/transformDatesToString.ts
+++ packages/server/test/utils/transformDatesToString.ts
@@ -1,42 +1,37 @@
-import { BasicObjectType } from "@/src/@types/app";
 import { isObject } from "@/src/utils/isObject";
 
-type EntityDatesTransformed<T extends BasicObjectType> = {
-  [K in keyof T]: T[K] extends Date ? string : T[K];
+type DateToString<T> = T extends Date ? string : T;
+
+type EntityDatesTransformed<T extends {}> = {
+  [K in keyof T]: DateToString<T[K]>;
 };
 
 /**
  * Necessário para testes porque TypeORM retorna dates como object,
  * porém na api graphql serializa como string
  */
-export function transformEntitiesDatesToString<T>(
+export function transformEntitiesDatesToString<T extends {}>(
   entities: T[],
 ): Array<EntityDatesTransformed<T>> {
-  const copy = [...entities];
-
-  copy.forEach((entity, index) => {
-    copy[index] = transformEntityDatesToString(entity) as any;
-  });
-
-  return copy as any;
+  return entities.map(entity => transformEntityDatesToString(entity));
 }
 
 export function transformEntityDatesToString<T extends {}>(
   entity: T,
 ): EntityDatesTransformed<T> {
-  const copy = { ...entity };
+  const copy: any = { ...entity };
 
   for (const key in copy) {
     const value = copy[key];
 
     if (value instanceof Date) {
-      copy[key] = value.toISOString() as any;
-    }
-
-    if (isObject(value)) {
-      copy[key] = transformEntityDatesToString(value) as any;
+      copy[key] = value.toISOString();
+    } else if (Array.isArray(value)) {
+      copy[key] = transformEntitiesDatesToString(value);
+    } else if (isObject(value)) {
+      copy[key] = transformEntityDatesToString(value);
     }
   }
 
-  return copy as any;
+  return copy;
 }
