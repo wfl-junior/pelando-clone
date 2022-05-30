00e0df1416cb4d8de72da5da2722a81a65c51682
Wallace Júnior <wflj1997@hotmail.com>
2022-05-30 14:54:51 -0300

    adicionar foreign keys na entity UserProductVote e adicionar createdAt aleatórios ao product.data para seed

--- packages/server/src/entities/user-product-vote.entity.ts
+++ packages/server/src/entities/user-product-vote.entity.ts
@@ -3,25 +3,33 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
+  ManyToOne,
   PrimaryColumn,
-  UpdateDateColumn,
 } from "typeorm";
 import { UserProductVoteType } from "../graphql-types/enums/UserProductVoteType";
+import { Product } from "./product.entity";
+import { User } from "./user.entity";
 
 @Entity("user_product_vote")
 export class UserProductVote extends BaseEntity {
   @PrimaryColumn("uuid", { name: "user_id" })
   public readonly userId: string;
 
+  @ManyToOne(() => User, { onDelete: "CASCADE" })
+  @JoinColumn({ name: "user_id" })
+  public readonly user: User;
+
   @PrimaryColumn("uuid", { name: "product_id" })
   public readonly productId: string;
+
+  @ManyToOne(() => Product, { onDelete: "CASCADE" })
+  @JoinColumn({ name: "product_id" })
+  public readonly product: Product;
 
   @Column()
   public type: UserProductVoteType;
 
   @CreateDateColumn({ name: "created_at" })
   public readonly createdAt: Date;
-
-  @UpdateDateColumn({ name: "updated_at" })
-  public readonly updatedAt: Date;
 }
