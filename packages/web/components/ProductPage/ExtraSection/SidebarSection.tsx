f095b7e50b61176a79e873d9c992b7380352ad60
Wallace Júnior <wflj1997@hotmail.com>
2022-06-10 14:10:05 -0300

    fix para posição da SidebarSection de product page

--- packages/web/components/ProductPage/ExtraSection/SidebarSection.tsx
+++ packages/web/components/ProductPage/ExtraSection/SidebarSection.tsx
@@ -13,51 +13,50 @@ export const SidebarSection: React.FC = () => {
   const { store, title, price } = useProductForProductPage();
 
   return (
-    <section
-      className="hidden flex-col gap-2 lg:flex lg:flex-col lg:gap-2"
-      style={{ gridArea: "sidebar" }}
-    >
-      <div className="bg-default-background border-default-border flex flex-col gap-2 rounded-lg border p-4">
-        <div className="flex items-center gap-2 font-bold">
-          <Link href="#">
-            <a className="text-secondary-foreground text-sm">{store.name}</a>
-          </Link>
+    <section className="hidden lg:block" style={{ gridArea: "sidebar" }}>
+      <div className="sticky top-[4.5rem] flex flex-col gap-2">
+        <div className="bg-default-background border-default-border flex flex-col gap-2 rounded-lg border p-4">
+          <div className="flex items-center gap-2 font-bold">
+            <Link href="#">
+              <a className="text-secondary-foreground text-sm">{store.name}</a>
+            </Link>
 
-          <div className="bg-inactive-background flex items-center gap-1 rounded-full py-px pr-1.5 pl-1 text-[10px] uppercase md:py-0.5 md:pr-2 md:text-xs">
-            <CheckIcon className="w-3.5 md:w-4" />
-            <span>Loja Verificada</span>
+            <div className="bg-inactive-background flex items-center gap-1 rounded-full py-px pr-1.5 pl-1 text-[10px] uppercase md:py-0.5 md:pr-2 md:text-xs">
+              <CheckIcon className="w-3.5 md:w-4" />
+              <span>Loja Verificada</span>
+            </div>
+          </div>
+
+          <h2 className="text-lg font-bold">{title}</h2>
+
+          <div className="text-primary text-2xl font-bold">
+            {price > 0 ? (
+              <Fragment>
+                <small>R$</small>
+                {price.toLocaleString(undefined, {
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2,
+                })}
+              </Fragment>
+            ) : (
+              "Grátis"
+            )}
+          </div>
+
+          <div className="mt-4 flex flex-col gap-2">
+            <CouponButton />
+            <LinkButton />
+          </div>
+
+          <div className="mt-4 grid grid-cols-[minmax(0,1fr)_minmax(0,2fr)_minmax(0,1fr)] place-items-center gap-4">
+            <CommentButton />
+            <TemperatureSection />
+            <ShareButton />
           </div>
         </div>
 
-        <h2 className="text-lg font-bold">{title}</h2>
-
-        <div className="text-primary text-2xl font-bold">
-          {price > 0 ? (
-            <Fragment>
-              <small>R$</small>
-              {price.toLocaleString(undefined, {
-                minimumFractionDigits: 2,
-                maximumFractionDigits: 2,
-              })}
-            </Fragment>
-          ) : (
-            "Grátis"
-          )}
-        </div>
-
-        <div className="mt-4 flex flex-col gap-2">
-          <CouponButton />
-          <LinkButton />
-        </div>
-
-        <div className="mt-4 grid grid-cols-[minmax(0,1fr)_minmax(0,2fr)_minmax(0,1fr)] place-items-center gap-4">
-          <CommentButton />
-          <TemperatureSection />
-          <ShareButton />
-        </div>
+        <AdSkeleton className="h-[250px]" aria-hidden="true" />
       </div>
-
-      <AdSkeleton className="h-[250px]" aria-hidden="true" />
     </section>
   );
 };
--- packages/web/components/ProductPage/ExtraSection/index.tsx
+++ packages/web/components/ProductPage/ExtraSection/index.tsx
@@ -6,7 +6,7 @@ import { SidebarSection } from "./SidebarSection";
 import { StoresSection } from "./StoresSection";
 
 export const ExtraSection: React.FC = () => (
-  <div className="grid-template-areas-product-page-extra grid gap-4 lg:container lg:grid-cols-[auto_280px] lg:gap-x-6 xl:grid-cols-[auto_320px]">
+  <div className="grid-template-areas-product-page-extra grid gap-4 lg:container lg:grid-cols-[auto_280px] lg:grid-rows-[auto_1fr_auto] lg:gap-x-6 xl:grid-cols-[auto_320px]">
     <div className="flex flex-col gap-4" style={{ gridArea: "main" }}>
       <AlertsSection />
       <CommentsSection />
